2696ee0f2af0109e25cdb2aa039b7b35d3c21e92
Nate Hodges <natehodges206@gmail.com>
2019-10-30 15:18:47 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloNMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to recieve a secret message");
+            Console.WriteLine();
+            Console.ReadKey();
+            Console.WriteLine("Hello NMC!");
+        }
+    }
+}
